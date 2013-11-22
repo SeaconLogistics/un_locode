a442766fbbfc3ccb10ee2f070d1462fd62e6f72c
Rui Salgado <ruivfsalgado@gmail.com>
2013-11-22 14:40:29 +0000

    Add initial spec for querying locodes
    Implementation is WIP

--- lib/un_locode/locode.rb
+++ lib/un_locode/locode.rb
@@ -2,23 +2,19 @@ module UnLocode
   class Locode < BaseRecord
     belongs_to :country
 
-    module Scopes
+    def self.find_by_fuzzy_name name, limit = 10
+      where("name like ?", "%#{name}%").limit(limit)
+    end
 
-      def find_by_name name, limit = 10
+    # search_string - The string that will be used in the LOCODE search.
+    # function - Integer or :B that specifies the function of the location
+    # limit - Integer to specify how many locations you want
+    def find_by_name_and_function name, function, limit = 10
+      return [] unless search_string.is_a?(String)
+      return [] unless function.to_s =~ /^[1-7]{1}|:B{1}$/
 
-      end
-
-      # search_string - The string that will be used in the LOCODE search.
-      # function - Integer or :B that specifies the function of the location
-      # limit - Integer to specify how many locations you want
-      def find_by_name_and_function name, function, limit = 10
-        return [] unless search_string.is_a?(String)
-        return [] unless function.to_s =~ /^[1-7]{1}|:B{1}$/
-
-
-      end
 
     end
-    extend Scopes
+
   end
 end
--- spec/un_locode_spec.rb
+++ spec/un_locode_spec.rb
@@ -7,9 +7,31 @@ describe UnLocode::Locode do
 
     describe 'retrieving locodes by name' do
 
-      it 'finds by regular name'
+      let!(:city) { UnLocode::Locode.create(name: 'Eindhoven', name_wo_diacritics: 'Eeindhoven') }
+      let!(:other_city) { UnLocode::Locode.create(name: 'Weert') }
 
-      it 'searches the name against all 4 name fields'
+      subject { UnLocode::Locode.find_by_fuzzy_name(search_term) }
+
+      context 'exact match' do
+        let(:search_term) { city.name }
+        its(:first) { should eql(city) }
+        its(:count) { should eql(1) }
+      end
+
+      context 'fuzzy match' do
+        let(:search_term) { city.name[1..4] }
+        its(:first) { should eql(city) }
+        its(:count) { should eql(1) }
+      end
+
+      [:name, :name_wo_diacritics, :alternative_name, :alternative_name_wo_diacritics].each do |attr|
+        context "matches against the #{attr} name fields" do
+          let!(:city) { UnLocode::Locode.create(attr => 'Eindhoven') }
+          let(:search_term) { 'Eindhoven' }
+          its(:first) { should eql(city) }
+          its(:count) { should eql(1) }
+        end
+      end
 
     end
 
