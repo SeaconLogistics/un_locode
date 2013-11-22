5cdf534849300cbd1ccfd5f9983ac05af424611e
Niels Stevens <niels@kabisa.nl>
2013-11-22 17:08:04 +0100

    Initialize DB config on Module load

--- lib/un_locode.rb
+++ lib/un_locode.rb
@@ -1,5 +1,11 @@
 require 'active_record'
-require "un_locode/base_record"
-require "un_locode/version"
-require "un_locode/country"
-require "un_locode/locode"
+require_relative '../data/db'
+
+module UnLocode
+  UnLocode::DB.new().connect_to_db
+end
+
+require 'un_locode/base_record'
+require 'un_locode/version'
+require 'un_locode/country'
+require 'un_locode/locode'
